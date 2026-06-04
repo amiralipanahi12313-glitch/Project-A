2686c49473baeeb671b709a7c7bad4aa3ab27eee
amiralipanahi12313-glitch <amirali.panahi.12313@gmail.com>
2026-06-04 21:56:26 +0330

    Change default port from 19132 to 80

--- society.js
+++ society.js
@@ -1,7 +1,7 @@
 const bedrock = require('bedrock-protocol');
 
 const HOST = process.env.MC_HOST || 'a6dfc83dbf2282e2-151-115-164-120.serveousercontent.com';
-const PORT = parseInt(process.env.MC_PORT || '19132');
+const PORT = parseInt(process.env.MC_PORT || '80');
 
 class Citizen {
   constructor(name) {
